ae008c1c00a8d1451dabb708ea389ae604707652
Akash Maurya <monsterchilly03@protonmail.com>
2023-11-13 20:23:45 +0530

    Added Git command used

--- Tic-Tac-Toe/src/Main.java
+++ Tic-Tac-Toe/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        /*
+            git command
+            git init
+            git add <file_name>
+            git add .
+            git status
+            git log
+            git branch <branchName>
+            git checkout <BranchName>
+
+         */
     }
 }
